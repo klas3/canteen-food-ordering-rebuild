a7ee8cb47ebd297f41e81045b14831987178aafc
Ivan <sluvkann@polytechnic.poltava.ua>
2020-06-18 16:20:29 +0300

    Set limit for incoming request

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '50mb' }));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
